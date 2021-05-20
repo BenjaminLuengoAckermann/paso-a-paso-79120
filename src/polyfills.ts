31addec8d1f7af3f6b8ca1a6d3b6cd1f9c4550ad
BenjaminLuengoAckermann <benjaluengoa@gmail.com>
2021-05-20 15:46:08 -0300

    2 files modified

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -54,7 +54,6 @@
  * Zone JS is required by Angular itself.
  */
 import 'zone.js/dist/zone'; // Included with Angular CLI.
-import '@angular/localize/init';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
